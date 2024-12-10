2e70d4793eb4b6c14ff7d820b8493cbec5c99db3
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2024-12-10 19:22:13 +0000

    basic debugging functionality via qemu gdb server

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ make -Cbuild
 if [[ "$1" == "-bin" ]]; then
 
     echo "running qemu directly on the bin file"
-    qemu-system-i386 -kernel build/rros.bin -display gtk,zoom-to-fit=on
+    qemu-system-i386 -kernel build/rros.bin -display gtk,zoom-to-fit=on -s -S
 
 elif [[ "$1" == "-grub" ]]; then
 
@@ -16,7 +16,7 @@ elif [[ "$1" == "-grub" ]]; then
     cp build/rros.bin build/isodir/boot/rros.bin
     cp extras/grub.cfg build/isodir/boot/grub/grub.cfg
     grub-mkrescue -o build/rros.iso build/isodir
-    qemu-system-i386 -cdrom build/rros.iso -display gtk,zoom-to-fit=on
+    qemu-system-i386 -cdrom build/rros.iso -display gtk,zoom-to-fit=on -s -S
 
 elif [[ "$1" == "-bochs" ]]; then
     echo "running via grub MB option"
